790330fa7382af55513c4f01bb57f41f91742d4b
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-09-10 21:09:32 +0200

    Fix "expected 1 blank line, found 0"
    Bug: T203951
    Change-Id: I536acb682f1719874ea65df60fa5981da53b37c9

--- trackersite/tracker/models.py
+++ trackersite/tracker/models.py
@@ -111,6 +111,7 @@ class DecimalRangeField(models.DecimalField):
     def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
         self.min_value, self.max_value = min_value, max_value
         models.DecimalField.__init__(self, verbose_name, name, **kwargs)
+
     def formfield(self, **kwargs):
         defaults = {'min_value': self.min_value, 'max_value': self.max_value }
         defaults.update(kwargs)
