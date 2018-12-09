48cd97724ac008cf138b2b9141b5cb342681975f
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-12-09 19:00:46 +0100

    Add a way to make authorized API requests
    Bug: T211508
    Change-Id: I67899f423d67f2acc88ac3593a461fb4879ba44c

---
+++ trackersite/socialauth/api.py
@@ -0,0 +1,29 @@
+from requests_oauthlib import OAuth1
+import requests
+from django.conf import settings
+
+
+class MediaWiki():
+    def __init__(self, user, api_url):
+        self.user = user
+        self.api_url = api_url
+
+        provider = user.social_auth.filter(provider="mediawiki")
+        if len(provider) == 1:
+            self.tokens = provider.get().extra_data.get('access_token')
+        else:
+            self.tokens = None
+        if self.tokens is None:
+            raise ValueError("Given user isn't connected with any MediaWiki account.")
+
+    def authorized_request(self, payload):
+        return requests.post(
+            self.api_url,
+            payload,
+            auth=OAuth1(
+                settings.SOCIAL_AUTH_MEDIAWIKI_KEY,
+                settings.SOCIAL_AUTH_MEDIAWIKI_SECRET,
+                self.tokens.get('oauth_token'),
+                self.tokens.get('oauth_token_secret')
+            )
+        )
