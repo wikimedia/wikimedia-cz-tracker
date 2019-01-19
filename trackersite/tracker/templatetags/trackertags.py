25544fed52a7c60fc3e9e375a1a17b534e96e6e2
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-01-19 16:16:53 +0100

    Create links from mentioned users
    I'm afraid many users don't know that doing @username in a comment
    will notify the user about the comment, so I decided to create a link
    from that username to make it clear Tracker recognize this syntax.
    
    As soon as users know Tracker recognizes this syntax, they might
    think (correctly) that it emails the user and acts as a ping feature.
    
    Change-Id: Icf681701f929b03669270d323cb3b66ee7aa8484

--- trackersite/tracker/templatetags/trackertags.py
+++ trackersite/tracker/templatetags/trackertags.py
@@ -3,7 +3,8 @@ from django import template
 from django.utils.safestring import mark_safe
 from django.conf import settings
 from django.utils.formats import number_format
-from tracker.models import Ticket
+from tracker.models import Ticket, User
+from django.core.urlresolvers import reverse
 import re
 import bleach
 
@@ -32,4 +33,12 @@ def tracker_rich_text(value):
         ticket = Ticket.objects.get(id=ticket_id)
         text = '<a href="%s">%s</a>' % (ticket.get_absolute_url(), "%s: %s" % (match, ticket.name))
         value = value.replace(match, text)
+
+    usersmentioned = re.findall(r'@([-a-zA-Z0-9_.]+)', value)
+    for user_name in usersmentioned:
+        users = User.objects.filter(username=user_name)
+        if len(users) == 1:
+            user_detail_url = reverse('user_detail', kwargs={'username': users[0].username})
+            value = value.replace('@%s' % user_name, '<a href="%s">@%s</a>' % (user_detail_url, user_name))
+
     return mark_safe(bleach.clean(value, settings.ALLOWED_TAGS, settings.ALLOWED_ATTRIBUTES))
