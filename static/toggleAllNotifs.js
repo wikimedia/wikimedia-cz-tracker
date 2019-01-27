8650e3161373765ae28d6c22ee25d644119ea71b
3to1null <nathanklumpenaar10@outlook.com>
2019-01-27 11:14:09 +0100

    Small fix: check 'All' notification box if all other boxes are selected on page load.
    Change-Id: Ib177fed321f9a714b165a5196133c7db6c27d9b4

--- static/toggleAllNotifs.js
+++ static/toggleAllNotifs.js
@@ -8,11 +8,18 @@ document.addEventListener( 'DOMContentLoaded', () => {
 		} );
 	} );
 
+	// looks at all the checkboxes and (un)checks the 'all' checkbox. Is run on every checkbox change.
+	function checkAllButton() {
+		allButton.checked = !Array.from( otherNotifTypes ).some( checkbox => !checkbox.checked );
+	}
+
+	// Sets the all button to the right initial value on page load.
+	checkAllButton();
+
 	otherNotifTypes.forEach( ( checkbox ) => {
 		checkbox.addEventListener( 'change', () => {
 			// set "all" checkbox unchecked if some checkboxes are not checked
-			allButton.checked = !Array.from( otherNotifTypes )
-				.some( checkbox => !checkbox.checked );
+			checkAllButton();
 		} );
 	} );
 } );
