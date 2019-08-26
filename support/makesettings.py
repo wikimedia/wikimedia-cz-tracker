674959663e887f65a243cd43884c0e3c2ae2e6c1
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-08-26 23:11:46 +0200

    Add provision script
    This script allows users to quickly
    provision a Tracker instance.
    
    Also changes makesettings.py to provide
    a ready settings.py, including database
    configuration.
    
    Change-Id: I35e7402870603adab2d2759dd36b843c02baf620

--- support/makesettings.py
+++ support/makesettings.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 import django
 from django.template import Template, Context
@@ -30,6 +31,43 @@ def main():
     options = {}
     chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
     options['secret_key'] = get_random_string(50, chars)
+
+    # DB provider
+    # TODO: Add Oracle and postgresql_psycopg2
+    print('Select your database provider')
+    print('1) MySQL')
+    print('2) SQLite')
+    db_choice = int(input('Your choice: '))
+    print("We'll ask you few questions now. Press enter if you want the defaults.")
+    if db_choice == 1:
+        options['backend'] = 'mysql'
+        options['DB_NAME'] = input('Database name [tracer]: ') or 'tracker'
+        options['DB_USER'] = input('Database username [root]: ') or 'root'
+        options['DB_PASS'] = input('Database password [empty string]: ') or ''
+        options['DB_HOST'] = input('Database hostname [localhost]: ') or ''
+        options['DB_PORT'] = input('Database port [MySQL default]: ') or ''
+        print('Creating your database')
+        subprocess.call([
+            'mysql',
+            # Pass username
+            '-u', options['DB_USER'],
+            # Pass password
+            '-p' + options['DB_PASS'],
+            # Pass -h and hostname, but only if db_host is non-empty string
+            '-h ' + options['DB_HOST'] if options['DB_HOST'] else '',
+            # Same for db_port
+            '-P ' + options['DB_PORT'] if options['DB_PORT'] else '',
+            '-e',
+            'CREATE DATABASE %s' % options['DB_NAME']
+        ])
+    elif db_choice == 2:
+        options['backend'] = 'sqlite3'
+        default_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..', 'deploy', 'tracker.db'))
+        options['DB_NAME'] = input('Path to new database file [%s]: ' % default_path) or default_path
+        options['DB_USER'] = ''
+        options['DB_PASS'] = ''
+        options['DB_HOST'] = ''
+        options['DB_PORT'] = ''
 
     context = Context(options)
     target = open(target_path, 'wb')
