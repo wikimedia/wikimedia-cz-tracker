ac9c1446156ea5fae3aeb2b8366165d4aee84c27
Martin Urbanec <martin.urbanec@wikimedia.cz>
2019-12-20 14:57:04 +0100

    Add exception arg to permission_denied handler
    Change-Id: I7a9434b489cf3d9fb917d64e66a48acffa341602

--- trackersite/errors.py
+++ trackersite/errors.py
@@ -6,13 +6,11 @@ from django.template import TemplateDoesNotExist, loader
 
 # TODO: Remove this override when Django version > 1.9
 # Author: proxy https://stackoverflow.com/a/34225705
-def permission_denied(request, template_name='403.html'):
-    _, value, _ = sys.exc_info()
-
+def permission_denied(request, exception, template_name='403.html'):
     try:
         template = loader.get_template(template_name)
     except TemplateDoesNotExist:
         return http.HttpResponseForbidden('<h1>403 Forbidden</h1>', content_type='text/html')
     return http.HttpResponseForbidden(
-        template.render(request=request, context={'exception': value})
+        template.render(request=request, context={'exception': exception})
     )
