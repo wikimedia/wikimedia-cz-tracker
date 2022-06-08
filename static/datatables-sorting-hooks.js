735d68520759685ecaa3c8f6aee658b700b4c82e
Martin Urbanec <martin.urbanec@wikimedia.cz>
2022-06-09 00:33:15 +0200

    datatables: Stop using date-with-none
    Let's switch to the default 'date'.
    
    Change-Id: Ia96c3bf9f5870b1afbc491a961a02352eaa632b0

--- static/datatables-sorting-hooks.js
+++ static/datatables-sorting-hooks.js
@@ -1,22 +1,4 @@
 ( function () {
-	function dateWithNoneToOrder( date ) {
-		var datePattern = /\d{4}-\d{2}-\d{2}/;
-		if ( datePattern.test( date ) ) {
-			return parseInt( String( date ).replace( '-', '' ) );
-		}
-		return 0;
-	}
-
-	$.fn.dataTableExt.oSort[ 'date-with-none-asc' ] = function ( a, b ) {
-		var ordA = dateWithNoneToOrder( a ),
-			ordB = dateWithNoneToOrder( b );
-		return ordA - ordB;
-	};
-	$.fn.dataTableExt.oSort[ 'date-with-none-desc' ] = function ( a, b ) {
-		var ordA = dateWithNoneToOrder( a ),
-			ordB = dateWithNoneToOrder( b );
-		return ordB - ordA;
-	};
 	$.fn.dataTableExt.oSort[ 'money-pre' ] = function ( a ) {
 		a = a.toString();
 		if ( a === '' ) {
